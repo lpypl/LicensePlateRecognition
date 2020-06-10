4933c0338c046be4163db5c6cd1faaaf164b460b
into233 <slomovbus@sina.com>
2020-06-10 19:13:14 +0800

    组装ui和功能模块,还差字符识别功能

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QFileDialog, QMessageBox, QLabel, QSizePolicy
 from PyQt5 import uic, QtCore
 from PyQt5.QtGui import QIcon, QPalette, QPixmap, QImage
+from pretreatment import *
+from licenseLocator import *
+from charSegment import *
 
 
 class Ui(QMainWindow):
@@ -15,8 +18,8 @@ class Ui(QMainWindow):
             'btnOpen', 'btnSave', 'btnPictureGray', 'btnGrayScales',
             'btnMedianFilter', 'btnLicenceGray', 'btnLicenceBinary',
             'btnCharSplit', 'btnEdgeDetection', 'btnLocation',
-            'btnCharIdentify', 'imgLoad', 'imgSplit', 'imgChar0',
-            'imgChar1', 'imgChar2', 'imgChar3', 'imgChar4', 'imgChar5', 
+            'btnCharIdentify', 'imgLoad', 'imgLocate', 'imgSplit', 'imgChar0',
+            'imgChar1', 'imgChar2', 'imgChar3', 'imgChar4', 'imgChar5',
             'imgChar6',
         ]
         for widget in widgets:
@@ -59,12 +62,28 @@ class Ui(QMainWindow):
     def __openImageAndShow(self, filename, label):
         m_image = QImage(filename)
         if m_image.isNull():
-            QMessageBox.information(self, "Image Viewer",
+            QMessageBox.information(self, "Error",
                                     "无法加载 %s." % filename)
             return
+        self.__showImageInLabel(m_image, label)
+
+        bgrImage = cv2.imread(filename)
+
+        self.pt = Pretreatment(bgrImage)
+        self.ll = LicenseLocator(bgrImage)
+        self.licenseImage = self.ll.getLicenseImage()
+        if(self.licenseImage is not None):
+            self.splitter = CharSplitter(self.licenseImage)
+
+    def __showImageInLabel(self, m_image: QImage, label):
+        if(m_image is None):
+            print("__showImageInLabel m_image is None")
+            return
+        label.clear()
+
         lebalWidth = label.frameGeometry().width()
         lebalHeight = label.frameGeometry().height()
-        print(lebalWidth, lebalHeight)
+        print('label width and height:',lebalWidth, lebalHeight)
         m_image.scaled(lebalWidth,
                        lebalHeight,
                        aspectRatioMode=QtCore.Qt.KeepAspectRatio,
@@ -74,9 +93,80 @@ class Ui(QMainWindow):
         label.setScaledContents(True)
         label.setPixmap(QPixmap.fromImage(m_image))
 
+    def btnPictureGrayPressed(self):
+        if(hasattr(self, 'pt')):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.pt.getGrayImage()), self.imgLoad)
+
+    def btnGrayScalesPressed(self):
+        if(hasattr(self, 'pt')):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.pt.getHistEqImage()), self.imgLoad)
+
+    def btnMedianFilterPressed(self):
+        if(hasattr(self, 'pt')):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.pt.getMedianImage()), self.imgLoad)
+
+    def btnEdgeDetectionPressed(self):
+        if(hasattr(self, 'll')):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.ll.getRectImage()), self.imgLoad)
+
+    def btnLocationPressed(self):
+        if(hasattr(self, 'll')):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.ll.getLicenseImage()), self.imgLocate)
+
+    def CNp2QImage(self, image):
+        """
+            convert np image to QImage
+        """
+        if(image is None):
+            print("CNp2QImage image is None")
+            return None
+        print('convert to QImage, (height width):', image.shape, type(image), image.data)
+
+        data = image.data
+        height, width, *channel = image.shape
+        bytesPerLine = 3 * width
+        if(isinstance(data, memoryview)):
+            data = data.tobytes()
+
+        if(len(channel) == 0):
+            return QImage(data, width, height, width, QImage.Format_Grayscale8)
+        elif(channel[0] == 3):
+            # rgbSwapped显示bgr
+            return QImage(data, width, height, width * 3, QImage.Format_RGB888).rgbSwapped()
+        else:
+            QMessageBox.information(self, "Error",
+                                    "图片转换失败, channel必须是0或3。")
+
+    def btnLicenceGrayPressed(self):
+        if(hasattr(self, "splitter")):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.splitter.getGrayImage()), self.imgLocate)
+
+    def btnLicenceBinaryPressed(self):
+        if(hasattr(self, "splitter")):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.splitter.getBinaryImage()), self.imgLocate)
+
+    def btnCharSplitPressed(self):
+        if(hasattr(self, "splitter")):
+            self.__showImageInLabel(self.CNp2QImage(
+                self.splitter.getSegmentImage()), self.imgSplit)
+
+            charImages = self.splitter.getCharImages()
+            print(len(charImages))
+            for n in range(0, 7):
+                self.__showImageInLabel(self.CNp2QImage(
+                    charImages[n]), getattr(self, 'imgChar' + str(n)))
+
     def btnSavePressed(self):
-        #TODO what is this function does
+        # TODO what is this function does
         print("btnSavePressed")
+
 
 def main():
     app = QApplication(sys.argv)
